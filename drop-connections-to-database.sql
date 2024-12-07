8266b33f5c00c72cc8706d78136bcf0243cd1bf1
Joachim Køcher Kelsen <71125597+jokk-itu@users.noreply.github.com>
2024-12-07 21:55:05 +0100

    Create drop-connections-to-database.sql

---
+++ drop-connections-to-database.sql
@@ -0,0 +1,9 @@
+USE [master];
+
+DECLARE @database varchar(200) = 'NAME OF DATABASE TO DROP';
+DECLARE @kill varchar(8000) = '';  
+SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), session_id) + ';'  
+FROM sys.dm_exec_sessions
+WHERE database_id  = db_id(@database)
+
+EXEC(@kill);
